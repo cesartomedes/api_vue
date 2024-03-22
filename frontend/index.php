9bce8e5ad93f33574d066b7493968862785d27b4
cesartomedes <cesartomedes@gmail.com>
2024-03-21 21:46:55 -0400

    add vue and axios

--- frontend/index.php
+++ frontend/index.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
     <title>Document</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </head>
 <body>
-    
+    soy el frontend
 </body>
 </html>
