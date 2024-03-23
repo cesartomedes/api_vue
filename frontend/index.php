d2a16323cf4be88e8db68679fd543549956b9196
cesartomedes <cesartomedes@gmail.com>
2024-03-22 22:30:00 -0400

    Add Vue and Axios CDN and consume for URL

--- frontend/index.php
+++ frontend/index.php
@@ -1,14 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Document</title>
+    <title>APIREst with PHP-VUE</title>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </head>
+
 <body>
-    soy el frontend
+ 
+    <div id="app">
+        <h1>Alumnos</h1>
+        <ul>
+            <li v-for="alumno in alumnos" :key="alumno.id">
+                {{ alumno.nombres }} - {{ alumno.apellidos }}
+            </li>
+        </ul>
+
+    </div>
+    <script>
+        const {createApp, ref, onMounted} = Vue;
+        const apiUrl = 'http://localhost:8080';
+        const app = createApp({
+            setup(){
+                const alumnos = ref([]);
+                const obtenerAlumnos=async() =>{
+                    const respuesta = await axios.get(apiUrl);
+                    alumnos.value=respuesta.data;
+                };
+                onMounted(()=>{
+                    obtenerAlumnos();
+                });
+                return{alumnos };
+            }
+        });
+
+        app.mount('#app');
+    </script>
 </body>
+
 </html>
