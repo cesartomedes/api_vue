ed19f2d08f29f3d06806dc5fda83e3ead7aab13f
cesartomedes <cesartomedes@gmail.com>
2024-04-04 09:56:00 -0400

    add back and front

--- backend/index.php
+++ backend/index.php
@@ -11,13 +11,24 @@ R::setup('mysql:host=localhost;dbname=aplicacion', 'root', '');
 // Create Router instance
 $router = new \Bramus\Router\Router();
 
+$router->options('.*', function(){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers:Content-Type, Authorization, X-Requested-with');
+    exit();
+});
+
 $router->get('/', function () {
 
     $alumnos = R::find('alumnos');
-
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-
     echo (json_encode(R::exportAll($alumnos)));
+});
+$router->post('/', function(){
+    $data = json_decode(file_get_contents('php://input'), true);
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    print_r($data);
 });
 $router->run();
--- frontend/index.php
+++ frontend/index.php
@@ -11,8 +11,16 @@
 </head>
 
 <body>
- 
+
     <div id="app">
+        <form @submit.prevent="crearAlumno">
+            Nombre:
+            <input type="text" name="nombres" v-model="nuevoAlumno.nombres" id="nombres">
+            Apellido:
+            <input type="text" name="apellidos" v-model="nuevoAlumno.apellidos" id="apellidos">
+            <button type="submit">Agregar Alumno</button>
+
+        </form>
         <h1>Alumnos</h1>
         <ul>
             <li v-for="alumno in alumnos" :key="alumno.id">
@@ -22,19 +30,41 @@
 
     </div>
     <script>
-        const {createApp, ref, onMounted} = Vue;
+        const {
+            createApp,
+            ref,
+            onMounted
+        } = Vue;
         const apiUrl = 'http://localhost:8080';
         const app = createApp({
-            setup(){
+            setup() {
                 const alumnos = ref([]);
-                const obtenerAlumnos=async() =>{
+
+                const nuevoAlumno = ref({
+                    nombres: '',
+                    apellidos: ''
+                });
+
+                const obtenerAlumnos = async () => {
                     const respuesta = await axios.get(apiUrl);
-                    alumnos.value=respuesta.data;
+                    alumnos.value = respuesta.data;
                 };
-                onMounted(()=>{
+                const crearAlumno = async () => {
+                    try {
+                        const respuesta = await axios.post(apiUrl, nuevoAlumno.value);
+                        alumnos.value.push(respuesta.data);
+                        nuevoAlumno.value={nombres:"", apellidos:""};
+                      
+                    } catch (error) {
+                        console.log("Error al crear el alumno", error)
+                    }
+                }
+                onMounted(() => {
                     obtenerAlumnos();
                 });
-                return{alumnos };
+                return {
+                    alumnos, crearAlumno, nuevoAlumno
+                };
             }
         });
 
